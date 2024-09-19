797addadde544967d66f319c0c0e06227eb1cb96
Kolaysik <tolstyhnikolai@gmail.com>
2024-09-19 17:52:59 +0300

    Задание 3

--- Lab_3.c
+++ Lab_3.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
 #include <locale.h>
 #include <stdlib.h>
-#define D1 2.54
-#define D2 2.32166
 
 
 int input_num() {
-	int dym;
-	float result1, result2;
-	scanf("%d", &dym);
-	result1 = D1 * dym;
-	result2 = D2 * dym;
-	printf("%d англ. дюймов - это %.1f см\n", dym, result1);
-	printf("%d исп. дюймов - это %.1f см\n", dym, result2);
+	int a, b;
+
+	scanf("%d", &a);
+	scanf("%d", &b);
+	system("pause");
+	puts("-------------------------");
+	puts("| a + b\t| a * b\t| a - b\t|");
+	puts("-------------------------");
+	printf("| %d + %d\t| %d * %d\t| %d - %d\t|\n", a, b, a, b, a, b);
+	puts("-------------------------");
+	printf("| %d\t| %d\t| %d\t|", a + b, a * b, a - b);
+	
+
+
+
 }
 
 void main() {
 	setlocale(LC_ALL, "RUS");
 	input_num();
-	system("pause");
 
 }
